09be4566a6d530475e87cf6693d06ee5a4392ce4
Daniel Nogueira <daniel.00msn@gmail.com>
2024-05-07 21:04:10 -0300

    inicializando projeto

---
+++ src/test/java/com/github/localizationAPI/LocalizationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.localizationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
