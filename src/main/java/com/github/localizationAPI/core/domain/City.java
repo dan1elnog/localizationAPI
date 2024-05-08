54caf2d9c923b29b6d5b336189fd489b1def0688
Daniel Nogueira <daniel.00msn@gmail.com>
2024-05-07 21:33:57 -0300

    Criando entidade 'cidade'

--- src/main/java/com/github/localizationAPI/core/domain/City.java
+++ src/main/java/com/github/localizationAPI/core/domain/City.java
@@ -1,4 +1,34 @@
 package com.github.localizationAPI.core.domain;
 
 public class City {
+
+    private Long id;
+
+    private String name;
+
+    private Long population;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
 }
--- src/main/java/com/github/localizationAPI/dataprovider/repository/entity/CityEntity.java
+++ src/main/java/com/github/localizationAPI/dataprovider/repository/entity/CityEntity.java
@@ -1,4 +1,26 @@
 package com.github.localizationAPI.dataprovider.repository.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Table(name = "city")
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CityEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @Column(name = "population")
+    private Long population;
 }
