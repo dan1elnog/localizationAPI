d96e92e3d1a236e8613f085cf2c006c7f8c6800c
Daniel Nogueira <daniel.00msn@gmail.com>
2024-05-11 16:22:43 -0300

    Criando fluxo completo de criação de cidades

--- src/main/java/com/github/localizationAPI/dataprovider/repository/mapper/CityEntityMapper.java
+++ src/main/java/com/github/localizationAPI/dataprovider/repository/mapper/CityEntityMapper.java
@@ -1,4 +1,14 @@
 package com.github.localizationAPI.dataprovider.repository.mapper;
 
-public class CityEntityMapper {
+import com.github.localizationAPI.core.domain.City;
+import com.github.localizationAPI.dataprovider.repository.entity.CityEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface CityEntityMapper {
+
+    CityEntity INSTANCE = Mappers.getMapper(CityEntity.class);
+
+    CityEntity toCityEntity(City city);
 }
--- src/main/java/com/github/localizationAPI/entrypoint/controller/request/InsertCityRequest.java
+++ src/main/java/com/github/localizationAPI/entrypoint/controller/request/InsertCityRequest.java
@@ -1,4 +1,13 @@
 package com.github.localizationAPI.entrypoint.controller.request;
 
-public record InsertCityRequest() {
-}
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record InsertCityRequest(
+
+        @NotEmpty(message = "The \"name\" field cannot be null or empty")
+        String name,
+
+        @NotNull(message = "The \"population\" field cannot be null or empty")
+        Long population
+) {}
