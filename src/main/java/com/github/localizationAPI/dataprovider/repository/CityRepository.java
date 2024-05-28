e64b4a0998dcfd711a073e3f99e2bd36d0681bfd
Daniel Nogueira <daniel.00msn@gmail.com>
2024-05-27 22:47:26 -0300

    Adicionando projection de cidade

--- src/main/java/com/github/localizationAPI/dataprovider/repository/CityRepository.java
+++ src/main/java/com/github/localizationAPI/dataprovider/repository/CityRepository.java
@@ -1,14 +1,19 @@
 package com.github.localizationAPI.dataprovider.repository;
 
 import com.github.localizationAPI.dataprovider.repository.entity.CityEntity;
+import com.github.localizationAPI.dataprovider.repository.projection.CityProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CityRepository extends JpaRepository<CityEntity, Long>, JpaSpecificationExecutor<CityEntity> {
 
     Page<CityEntity> findByNameContaining(String name, Pageable pageable);
+
+    @Query(value ="SELECT c.id, c.name FROM city" , nativeQuery = true)
+    CityProjection getCityProjection();
 }
---
+++ src/main/java/com/github/localizationAPI/dataprovider/repository/projection/CityProjection.java
@@ -0,0 +1,8 @@
+package com.github.localizationAPI.dataprovider.repository.projection;
+
+public interface CityProjection {
+
+    Long getId();
+
+    String getName();
+}
